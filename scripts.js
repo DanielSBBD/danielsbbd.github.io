38c1bbe443220fa0d94e34fb5bcf762709138eb4
Daniel Shuttleworth <daniels@bbd.co.za>
2023-01-31 15:19:28 +0200

    Made troll button more evaisive

--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
-window.onload(getJoke())
+window.onload = getJoke;
 
 function toggleNav() {
     if (document.getElementById("navbar").style.top == "100px") {
@@ -21,11 +21,30 @@ function leaveAction(id) {
     document.getElementById("info"+id).style['border-color'] = "rgba(0, 0, 0, 0)";
 }
 
+var x = 0;
+var y = 0;
+
+function moveButton() {
+    let xOff, yOff = 0;
+    do {
+        theta = Math.random()*2*Math.PI;
+        xOff = 300*Math.cos(theta);
+        yOff = 300*Math.sin(theta);
+    } while((x + xOff) < -window.innerWidth*0.4 || (x + xOff) > window.innerWidth*0.4 || (y + yOff) < -window.innerHeight*0.2 || (y + yOff) > window.innerHeight*0.6)
+
+    x += xOff;
+    y += yOff;
+    document.getElementsByTagName("button")[0].style.transform = `translate(${x}px,${y}px)`;
+}
+
 async function getJoke() {
-    const response = await fetch('https://api.chucknorris.io/jokes/random');
-    const data = await response.json()
-    let quote = data.value
-    quote = quote.replaceAll("Chuck Norris", "Skelly")
-    quote = quote.replaceAll("\'", "\'s")
-    document.getElementById("quote").innerHTML = quote
+    if(document.URL.includes("index.html")) {
+        const response = await fetch('https://api.chucknorris.io/jokes/random');
+        const data = await response.json();
+        let quote = data.value;
+        const reg = /[Cc]huck(?: [Nn]orris)?/g;
+        quote = quote.replaceAll("s\'", "s\'s");
+        quote = quote.replaceAll(reg, "Skelly");
+        document.getElementById("quote").innerHTML = quote;
+    }
 }
