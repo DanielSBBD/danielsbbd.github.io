c5b4e12038e612f081a6f81df8bada762aab2bec
Daniel Shuttleworth <daniels@bbd.co.za>
2023-02-01 16:57:05 +0200

    Added calculator backend

--- scripts.js
+++ scripts.js
@@ -37,6 +37,100 @@ function moveButton() {
     document.getElementsByTagName("button")[0].style.transform = `translate(${x}px,${y}px)`;
 }
 
+function calculate() {
+    el = document.getElementById("input");
+    input = el.value;
+    output = "Invalid Input";
+
+    // Remove spaces
+    input = input.replace(/\s/g, "");
+
+    //Check that all characters are valid
+    if(/^[\d\(\)\/*\-+.]+$/.test(input)) {
+        input = input.split("");
+        input = group(input);
+        output = '='+evaluate(input);
+    } 
+    el.value = output;
+}
+
+function evaluate(input) {
+    let start = [];
+    let bCount = 0;
+
+    // Process brackets
+    for(let i=0;i<input.length;i++) {
+        if(input[i]=='(') {
+            start.push(i);
+            bCount++;
+        } else if(input[i]==')') {
+            let popped = start.pop()
+            subExp = input.slice(popped+1,i)
+            input.splice(popped, i-popped+1, evaluate(subExp)[0])
+            i -= i-popped;
+            bCount--;
+        }
+    }
+
+    // Verify valid expression
+    let expectNum = true;
+    for(let i=0;i<input.length;i++) {
+        if(expectNum) {
+            if(isNaN(input[i])) {
+                return "Syntax Error";
+            }
+            expectNum = false;            
+        } else {
+            if(!isNaN(input[i])) {
+                return "Syntax Error";
+            }
+            expectNum = true;
+        }
+    }
+    if(expectNum) {
+        return "Syntax Error";
+    }
+
+    // Process * and /
+    while(input.includes('*') || input.includes('/')) {
+        for(let i=1;i<input.length-1;i+=2) {
+            if(input[i]=='*') {
+                input.splice(i-1, 3, (input[i-1] * input[i+1]))
+            } else if(input[i]=='/') {
+                input.splice(i-1, 3, (input[i-1] / input[i+1]))
+            }
+        }
+    }
+
+    // Process + and -
+    while(input.includes('+') || input.includes('-')) {
+        for(let i=1;i<input.length-1;i+=2) {
+            if(input[i]=='+') {
+                input.splice(i-1, 3, (+input[i-1] + +input[i+1]))
+            } else if(input[i]=='-') {
+                input.splice(i-1, 3, (input[i-1] - input[i+1]))
+            }
+        }    
+    }
+
+    return input;
+}
+
+function group(arr) {
+    let out = [arr[0]];
+    let opReg = /^[+\-*\/]$/;
+    let numReg = /\d/;
+    for(let i=1;i<arr.length;i++) {
+        let isNum = !isNaN(out[out.length-1]+arr[i]);
+        if((i==1 && isNum) || (i>=2 && !(numReg.test(arr[i-2]) && opReg.test(arr[i-1]))) && isNum) {
+            out[out.length-1] += arr[i];
+        } else {
+            out.push(arr[i]);
+        }
+    }
+    return out;
+}
+
 async function getJoke() {
     if(document.URL.includes("index.html")) {
         const response = await fetch('https://api.chucknorris.io/jokes/random');
